e397681a1c8cab818a42f6c30341c56be571105a
DBENSAN <dftb97@gmail.com>
2023-01-09 23:47:51 -0800

    fix local storage functionality

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ $(function () {
   // Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour.
 
+
   $('.time-block').each(function () {
     // console.log(this);
     if (Number(this.id) === hourNow) {
@@ -19,12 +20,15 @@ $(function () {
     } else {
       $(this).removeClass('past present').addClass('future');
     }
+  
   })
 
 
   // Add a listener for click events on the save button.
   // Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements.
+
+  
 
   $(".saveBtn").on("click", function () {
     console.log(this);
@@ -34,7 +38,9 @@ $(function () {
     console.log(time); 
 
 
-    var savedData = [];
+    // var savedData = [];
+
+    // localStorage.setItem()
 
   })
 
